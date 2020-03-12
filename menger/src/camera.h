d4b03a2704e1381ca2a34443b9c1fa3e1a4cf5ea
Annan Wang <wan4401@me.com>
2020-03-12 18:11:51 -0500

    Update camera.h

--- menger/src/camera.h
+++ menger/src/camera.h
@@ -2,17 +2,34 @@
 #define CAMERA_H
 
 #include <glm/glm.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 
 class Camera {
 public:
 	glm::mat4 get_view_matrix() const;
-	// FIXME: add functions to manipulate camera objects.
+  glm::mat4 lookAt() const;
+  void switchMode();
+  void setMouseMovement(bool event);
+  bool getMouseMovement();
+  void setMousePos(double mouse_x, double mouse_y);
+  void rotateMouse(double mouse_x, double mouse_y);
+  void roll(bool left);
+  void zoomMouse(double mouse_y);
+  void zoomKey(bool forward);
+  void panHor(bool left);
+  void panVert(bool up);
 private:
 	float camera_distance_ = 3.0;
-	glm::vec3 look_ = glm::vec3(0.0f, 0.0f, -1.0f);
-	glm::vec3 up_ = glm::vec3(0.0f, 1.0, 0.0f);
-	glm::vec3 eye_ = glm::vec3(0.0f, 0.0f, camera_distance_);
+	glm::vec3 Camera_look = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 Camera_up = glm::vec3(0.0f, 1.0, 0.0f);
+	glm::vec3 Camera_eye = glm::vec3(0.0f, 0.0f, camera_distance_);
 	// Note: you may need additional member variables
+  double saved_mouse_x;
+  double saved_mouse_y;
+  bool fps_mode = false;
+  bool mouse_pressed = true; 
+  glm::vec3 Camera_center = glm::vec3(0.0f, 0.0f, 0.0f);
+  glm::vec3 Camera_tangent = glm::normalize(glm::cross(Camera_look, Camera_up));
 };
 
 #endif
